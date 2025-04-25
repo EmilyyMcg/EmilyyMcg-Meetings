19422e59627fe4e2962963828fa26b2b189e2220
EmilyyMcg <124791397+EmilyyMcg@users.noreply.github.com>
2025-04-25 13:01:55 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
 const express = require('express');
-const PeerServer = require('peer').PeerServer;
+const { ExpressPeerServer } = require('peer');
 const http = require('http');
+
+
+// Initialize Express app
 const app = express();
-const PORT = process.env.PORT || 9000;
 
 // Create an HTTP server
 const server = http.createServer(app);
 
-// Mount the PeerJS server on the /peerjs route
-const peerServer = PeerServer({
-    port: 9000, // or the port Render assigns you
-    path: '/peerjs',
-    cors: {
-        origin: 'https://learning-lounge-meetings.onrender.com/', // The domain of your frontend
-        methods: ['GET', 'POST']
-    }
+// Create a PeerJS server instance with debug enabled
+const peerServer = ExpressPeerServer(server, {
+    debug: true
 });
 
+// Mount the PeerJS server on the /peerjs route
+app.use('/peerjs', peerServer);
+
 app.use('/node_modules', express.static('node_modules'));
+
 
 
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
 
 // Start the server on port 9000
-app.listen(PORT, () => {
-    console.log(`Web server running on port ${PORT}`);
+server.listen(9000, () => {
+    console.log('Server is running on http://localhost:9000');
 });
-
 
